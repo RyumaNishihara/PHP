1f3545b795df070a130969840123e5ff4f2a8821
ryuma <ryuma.nishihara@gmail.com>
2022-01-23 12:34:08 +0900

    変数定義

--- index_test.php
+++ index_test.php
@@ -1,10 +1,18 @@
+
 <?php
   /*
-    複数行用コメントアウト
+  複数行用コメントアウト
   */
-  echo 'hello';
+  echo 'hello<br>';
+  
+  //変数定義（重複した値を変数化する）
+  $name = 'Bob';
+  echo 'hello, ' . $name . '<br>';
+  echo 'bye, ' . $name . '<br>';
+
+  $person_name = 'jin';
+  echo 'oshiro ' . $person_name . '<br>';
 ?>
 
-<!-- phpプログラムの後にHTMLが続かない場合は、?>の閉じタグをつけない（余分な半角スペースが入ってします）。 -->
-
 <!-- phpモードの外はHTMLの記述ができる -->
+<!-- phpプログラムの後にHTMLが続かない場合は、?>の閉じタグをつけない（余分な半角スペースが入ってします）。 -->
