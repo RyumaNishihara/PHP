363e365b0b9d8acf93c55e9e86ef4baee09374c2
ryuma <ryuma.nishihara@gmail.com>
2022-01-23 12:50:31 +0900

    文字列の操作

--- index_test.php
+++ index_test.php
@@ -12,6 +12,17 @@
 
   $person_name = 'jin';
   echo 'oshiro ' . $person_name . '<br>';
+
+  //文字列の操作
+  $a_name = 'Big';
+    // . を使って文字列を結合する。
+  echo '5hello, ' . $a_name . '<br>';
+    /*
+      ''と違って""の場合、中で変数を使うことができる。
+      文字の間で変数を使いたい場合{}を使う。
+      エスケープシーケンスを使うことができる。\n（option + ¥）(改行)
+    */
+  echo "6hel{$a_name}lo, $a_name\n";
 ?>
 
 <!-- phpモードの外はHTMLの記述ができる -->
