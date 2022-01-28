30a7982a36a0480b39dd0aeb2ba038551aeaf78d
ryuma <ryuma.nishihara@gmail.com>
2022-01-28 12:55:37 +0900

    配列と繰り返しpart2

--- index_test2.php
+++ index_test2.php
@@ -1,4 +1,26 @@
 <?php 
+  $ary1 = ['a', 'b', 'c'];
+  $ary2 = ['d', 'e', 'f'];
+  $ary3 = array_merge($ary1, $ary2);
+  print_r($ary3);
+  echo '<br>';
+  
+  $ary4 = [1, 2, 3, 5, 6, 3, 1, 3, 8];
+  $ary5 = array_count_values($ary4);
+  print_r($ary5);
+  echo '<br>';
+  echo $ary5[1];
+  echo '<br>';
+  $c = 0;
+  foreach($ary4 as $num) {
+    if($num === 3) {
+      $c++;
+    }
+  }
+  print_r($c);
+  
+
+  echo '<br>', '----------------------' . '<br>';
   //配列と繰り返し
   // $arry = array('taro', 'hanako', 'jito'); 配列をこの様に定義することもできる。
   $arry = ['taro', 'hanako', 'jiro'];
@@ -40,4 +62,30 @@
     echo '<div>', $i, ': ', $v, '</div>';
   }
 
+  // 配列と繰り返しpart2
+  $arry2 = [
+    ['table', 1000],
+    ['chair', 100],
+    ['bed', 10000],
+  ];
+
+  //配列の中の、配列の要素を指定
+  $arry2[1][1] = 500;
+
+  //指定した配列の一番最初の要素を削除する。
+  // array_shift($arry2);
+
+  //指定した配列の一番最後の要素を削除する。
+  // array_pop($arry2);
+
+  //指定した配列の、どこから~いくつ削除するか決めることができる。
+  //array_splice(指定する配列, どこから, いくつ消すか); どこからは、インデックス番号で指定している
+  //array_splice(指定する配列, どこから, いくつ消すか, 新しく挿入する値);
+  array_splice($arry2, 1, 1, [['splice', 111]]);
+  // print_r($arry2);
+
+  foreach($arry2 as $val) {
+    print_r($val) . '<br>';
+    echo "{$val[0]}は、{$val[1]}円です。", '<br>';
+  }
 ?>
