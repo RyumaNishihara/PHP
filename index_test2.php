62d4ff9ea997aa5326ff3f0ef2329a9b52afd1c1
ryuma <ryuma.nishihara@gmail.com>
2022-01-28 15:18:17 +0900

    コード一部yanbaru.phpに移動

--- index_test2.php
+++ index_test2.php
@@ -1,26 +1,4 @@
 <?php 
-  $ary1 = ['a', 'b', 'c'];
-  $ary2 = ['d', 'e', 'f'];
-  $ary3 = array_merge($ary1, $ary2);
-  print_r($ary3);
-  echo '<br>';
-  
-  $ary4 = [1, 2, 3, 5, 6, 3, 1, 3, 8];
-  $ary5 = array_count_values($ary4);
-  print_r($ary5);
-  echo '<br>';
-  echo $ary5[1];
-  echo '<br>';
-  $c = 0;
-  foreach($ary4 as $num) {
-    if($num === 3) {
-      $c++;
-    }
-  }
-  print_r($c);
-  
-
-  echo '<br>', '----------------------' . '<br>';
   //配列と繰り返し
   // $arry = array('taro', 'hanako', 'jito'); 配列をこの様に定義することもできる。
   $arry = ['taro', 'hanako', 'jiro'];
