d93af56ff3c8f8135e77041fd14c538afda04390
ryuma <ryuma.nishihara@gmail.com>
2022-02-02 13:28:11 +0900

    条件分岐を省略(三項演算子・null合体演算子)

--- index_test4.php
+++ index_test4.php
@@ -19,3 +19,31 @@ function counter($step = 1) {
 //関数内の処理は関数が実行されて初めて動く
 $num = 0;
 counter(2);
+
+echo '<br>-------------条件分岐を省略して記述してみよう--------------<br>';
+
+/*
+  * 三項演算子の使い方
+  * null合体演算子
+*/
+$arry = [
+  'key' => 10,
+];
+
+$arry2 = [
+  'key' => 20,
+];
+
+// if(isset($arry['key'])) {
+//   $arry['key'] *= 10;
+// } else {
+//   $arry['key'] = 1;
+// }
+
+//三項演算子
+$arry['key'] = isset($arry['key']) ? $arry['key'] *= 10 : 1;
+echo $arry['key'] . '<br>';
+
+//null合体演算子（入ってきた値がnullだったら1を返すという式になっている。）
+$arry2['key'] = $arry2['key'] ?? 1;
+echo $arry2['key'] . '<br>';
