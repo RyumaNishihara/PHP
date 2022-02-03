5fbf2c0daa99e50af6cdbfab91d023f6fe082271
ryuma <ryuma.nishihara@gmail.com>
2022-02-03 20:32:22 +0900

    PHPとWEB

---
+++ PHPとWEB/test.php
@@ -0,0 +1,11 @@
+
+- HTTPのGETとPOSTを使って、ブラウザからサーバーに対してデータを送信する実装を行う。
+- method属性を省略するとGET方式になる
+
+<form action="post.php" method="POST">
+  <input type="text" name="username">
+  <input type="password" name="pwd">
+  <input type="submit" value="ボタンを押してね">
+</form>
+
+
